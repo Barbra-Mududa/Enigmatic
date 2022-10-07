4cceb799b50d61da253d62d10a4bb3d35e03adaf
Barbra Mududa <barbra.mududa@student.moringaschool.com>
2022-10-07 16:54:31 +0300

    enable blog post

--- src/Components/Blog.js
+++ src/Components/Blog.js
@@ -1,16 +1,31 @@
-import React, { useState} from 'react'
+import React, { useState } from 'react'
 
 function Blog() {
-  const[blog, setBlog] = useState([]);
-  
-  // // const handleSubmit = (data) => {
+  const [title, setTitle] = useState('');
+  const [message, setMessage] = useState('');
 
-  // // }
+  const url = "http://localhost:3000/blog"
+  const formData = {title,message}
+
+  const handleAdd = (event) => {
+    event.preventDefault();
+    
+    fetch(url, {
+      method: 'POST',
+      headers:{'Content-Type':'application/json'},
+      body: JSON.stringify(formData)
+    })
+    .then((res) => res.json())
+    .then(()=>{
+      setTitle(" ")
+      setMessage(" ")
+    })
+  } 
 
   return(
-    <div className='lets-blog'>
+    <div className='lets-blog' onSubmit={handleAdd}>
         <h1>Blog on your experiences</h1>
-        <form id='blogForm'>
+        <form id='blogForm' onSubmit={handleAdd}>
             <label>Category</label>
             <select>
                 <option value="beginner">Beginner</option>
@@ -20,10 +35,11 @@ function Blog() {
             </select>
 
             <label>Title:</label>
-            <input type="text" placeholder=""/>
+            <input type="text" value={title} onChange={(event)=>setTitle(event.target.value)} required></input>
 
             <label>Blog message:</label>
-            <textarea required></textarea>
+            <textarea rows = "5" cols = "50"  value={message} onChange={(e)=>setMessage(e.target.value)} required>
+            Blog away...</textarea>
             
             <button id='blog-btn'>Post</button>
         </form>

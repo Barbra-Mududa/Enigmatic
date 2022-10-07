7750f97ecabd5ef23d03bcf3e616fed34b1e47cc
Barbra Mududa <barbra.mududa@student.moringaschool.com>
2022-10-07 07:24:20 +0300

    hiking-list structure

--- src/Components/HikingList.js
+++ src/Components/HikingList.js
@@ -1,21 +1,21 @@
-// import React from 'react'
+import React from 'react'
 
-// function HikingList({img}) {
+function HikingList({img}) {
 
-//   return (
-//      <div className="book-card">
-//        <div className="eventImg">
-//         <img src={img.image} alt="images" width={300}></img>
-//       </div>     
-//       <div className="eventTitle">
-//         <h2>{img.title}</h2>
-//       </div>
+  return (
+     <div className="book-card">
+       <div className="eventImg">
+        <img src={img.image} alt="images" width={300}></img>
+      </div>     
+      <div className="eventTitle">
+        <h2>{img.title}</h2>
+      </div>
 
-//       <div className="eventDescription">
-//         <p>{img.Description}</p>
-//       </div>
-//     </div>
-//  )
-// }
+      <div className="eventDescription">
+        <p>{img.Description}</p>
+      </div>
+    </div>
+ )
+}
 
-// export default HikingList
+export default HikingList

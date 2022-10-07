5ca2237f04c2f5c980b7f8b03159d083545db1bb
Barbra Mududa <barbra.mududa@student.moringaschool.com>
2022-10-07 10:45:40 +0300

    search component structure

--- src/Components/Events.js
+++ src/Components/Events.js
@@ -1,19 +1,31 @@
 import React, { useEffect, useState} from 'react';
 import Card from './Card';
+import Search from './Search';
 
 function Events() {
 const [images, setImages] = useState([]);
+const [ search,  setSearch] = useState([]);
 
 useEffect(() => {
     fetch("http://localhost:3000/images")
       .then((res) => res.json())
       .then((event) => {
         setImages(event);
+        setSearch(event)
     });
 }, []);
-        
+
+function filter(e) {
+    setImages(
+      search.filter((images) =>
+        images.title.toLowerCase().includes(e.target.value.toLowerCase())
+      )
+    );
+  }
+
   return (
     <div>
+        {/* <Search searchInput={filter}/> */}
         <Card images={images}/>
     </div>
   )
